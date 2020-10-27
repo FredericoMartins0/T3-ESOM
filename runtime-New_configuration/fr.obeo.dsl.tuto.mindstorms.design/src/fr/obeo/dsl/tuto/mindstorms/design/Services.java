c1a5da287e85c1c8a0a8d89c6e7811bf67ffdf09
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2020-10-27 19:11:13 -0300

    runtime adicionado

---
+++ runtime-New_configuration/fr.obeo.dsl.tuto.mindstorms.design/src/fr/obeo/dsl/tuto/mindstorms/design/Services.java
@@ -0,0 +1,25 @@
+package fr.obeo.dsl.tuto.mindstorms.design;
+
+import java.util.List;
+
+import fr.obeo.dsl.tuto.mindstorms.Choreography;
+import fr.obeo.dsl.tuto.mindstorms.Instruction;
+
+
+/**
+ * The services class used by VSM.
+ */
+public class Services {
+    
+    public Instruction getNextInstruction(Instruction instruction) {
+		Choreography parentChoreography=(Choreography)instruction.eContainer();
+		List<Instruction> actions=parentChoreography.getInstructions();
+		int position=actions.indexOf(instruction);
+		if (position==actions.size()-1) {
+			return null;
+		}
+		else {
+			return actions.get(position+1);
+		}
+	}
+}
